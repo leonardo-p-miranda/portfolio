c4c2925d26ff99e8d924efc19d14b22b9a87e73f
Leonardo Miranda <dev.leonardomiranda@gmail.com>
2024-12-16 17:13:35 -0300

    chore: Deploying with baseurl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // nuxt.config.ts
 export default defineNuxtConfig({
+  app: {
+    baseURL: "https://leonardo-p-miranda.github.io/portfolio",                            
+    head: {
+      title: "Leonardo Miranda",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: "Fullstack Developer",
+        },
+      ],
+    },
+  },
   css: ["@/assets/main.css"],
   modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
 

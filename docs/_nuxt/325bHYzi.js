a5e4445bff832b79efbb6b7fcb09d9f29d7b332b
Leonardo Miranda <dev.leonardomiranda@gmail.com>
2026-05-29 13:37:05 -0300

    chore(docs): rebuild static output with photo and about layout update

--- docs/_nuxt/325bHYzi.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./CbP9FgpO.js";import"./BKy29PzD.js";import"./C-v3KzvZ.js";import"./CxYJgpPD.js";import"./C1VQmFGW.js";export{o as default};
---
+++ docs/_nuxt/Cl7xI2Ms.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./BA7ShfRI.js";import"./CoNVNgU_.js";export{m as default};

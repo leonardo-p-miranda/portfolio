b965b740688fed56bda372280ac7608bd55b27be
Leonardo Miranda <dev.leonardomiranda@gmail.com>
2026-05-29 15:31:08 -0300

    Updates

--- _nuxt/BKaMphey.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./CziazNcb.js";import"./DpYkO-i3.js";import"./C-v3KzvZ.js";import"./CxYJgpPD.js";import"./Cu09VRZs.js";export{o as default};
---
+++ _nuxt/D6lUBzAI.js
@@ -0,0 +1,1 @@
+import s from"./CsQc90zs.js";import{e as o,z as u,J as f,n as m}from"./C4OtfIl5.js";import"./CxYJgpPD.js";const l=o({name:"Markdown",extends:s,setup(t){const{parent:e}=f(),{between:n,default:a}=u(),r=m(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:r,between:n,parent:e}}});export{l as default};
